f60d15b90a347ac7223df7b0fea226b39a4b524e
juliuspatrickpeji <juliuspatrick.amita.peji@gmail.com>
2025-10-16 14:38:34 +0800

    more settings

--- src/app/v3/page3/page.tsx
+++ src/app/v3/page3/page.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@/components/ui/button";
 
-export default function PlainPage() {
+export default function MoreSettingsPage() {
   return (
     <div className="min-h-screen bg-white p-6">
       <header className="border-b border-gray-200 mb-6 pb-3">
@@ -11,17 +11,17 @@ export default function PlainPage() {
       </header>
 
       <main>
-       <Button variant="outline" className="flex flex-col items-start justify-center gap-1 rounded-md bg-white text-red-500 mt-1 py-4 px-6 h-auto">
+       <Button variant="outline" className=" w-full flex flex-col items-start justify-center gap-1 rounded-md bg-white text-red-500 mt-1 py-4 px-6 h-auto">
   <span className="text-sm font-semibold leading-none">Permanently DELETE all accounts (OSA and STUDENT).</span>
   <span className="text-xs text-black leading-tight">This action cannot be undone. This will permanently delete all accounts and remove all data from our database.</span>
        </Button>
 
-  <Button variant="outline" className="flex flex-col items-start justify-center gap-1 rounded-md bg-white text-red-500 mt-1 py-4 px-6 h-auto">
+  <Button variant="outline" className=" w-full flex flex-col items-start justify-center gap-1 rounded-md bg-white text-red-500 mt-1 py-4 px-6 h-auto">
   <span className="text-sm font-semibold leading-none">Permanently DELETE all facial data ALL STUDENT.</span>
   <span className="text-xs text-black leading-tight">This action cannot be undone. This will permanently delete all facial data from our database. Students will be required to registered their facial data again.</span>
        </Button>
 
-         <Button variant="outline" className="flex flex-col items-start justify-center gap-1 rounded-md bg-white text-black mt-1 py-4 px-6 h-auto">
+         <Button variant="outline" className=" w-full flex flex-col items-start justify-center gap-1 rounded-md bg-white text-black mt-1 py-4 px-6 h-auto">
   <span className="text-sm font-semibold leading-none">Deactivate all accounts (OSA and STUDENT).</span>
   <span className="text-xs text-black leading-tight">Sets all account to INACTIVE. All accounts are still saved but inactive.</span>
        </Button>
